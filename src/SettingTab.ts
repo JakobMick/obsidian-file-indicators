69fec5b0f32b154946e0db8af91d00cfa85afb4b
Jakob Mick <jakobmick@live.de>
2023-10-16 23:35:30 +0200

    refactor: add src directory

--- src/SettingTab.ts
+++ src/SettingTab.ts
@@ -1,107 +1,9 @@
 import { App, Plugin, PluginSettingTab, Setting, } from 'obsidian';
 
-interface FileIndicatorsSettings {
-	defaultColor: string;
-    defaultShape: string;
-    indicators: (Indicator)[]
-}
+import FileIndicatorsPlugin from './main';
+import Indicator from './indicator';
 
-interface Indicator {
-    dataPath: string;
-    color: string;
-    shape: string
-}
-
-const DEFAULT_SETTINGS: FileIndicatorsSettings = {
-	defaultColor: '#8A5CF5',
-    defaultShape: 'CIRCLE',
-    indicators: [],
-}
-
-export default class FileIndicatorsPlugin extends Plugin {
-	settings: FileIndicatorsSettings;
-
-	async onload() {
-		await this.loadSettings();
-        
-        this.loadIndicators();
-
-        this.registerEvent(this.app.workspace.on("file-menu", (menu, file) => {
-            const indicator = this.settings.indicators.find((indicator) => indicator.dataPath == file.path);
-
-            if(!indicator) {
-                menu.addItem(item => item
-                    .setTitle("Add indicator")
-                    .setIcon("plus")
-                    .onClick(async () => {
-                        const indicator = {
-                            dataPath: file.path,
-                            color: this.settings.defaultColor,
-                            shape: this.settings.defaultShape,
-                        };
-        
-                        this.addIndicatorCSS(indicator);
-                        this.settings.indicators.push(indicator);
-                        await this.saveSettings();
-                    }));
-            } else {
-                menu.addItem(item => item
-                    .setTitle("Remove indicator")
-                    .setIcon("minus")
-                    .onClick(async () => {
-                        const indicator = {
-                            dataPath: file.path,
-                            color: this.settings.defaultColor,
-                            shape: this.settings.defaultShape,
-                        };
-        
-                        this.removeIndicatorCSS(indicator);
-                        const index = this.settings.indicators.findIndex((i) => i.dataPath == indicator.dataPath);
-                        this.settings.indicators.remove(this.settings.indicators[index])
-                        await this.saveSettings();
-                    }));
-            }
-        }));
-
-		this.addSettingTab(new FileIndicatorsSettingTab(this.app, this));
-	}
-
-	onunload() {
-        this.unloadIndicators();
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-
-    loadIndicators() {
-        this.settings.indicators.forEach((i) => this.addIndicatorCSS(i))
-    }
-    
-    unloadIndicators() {
-        this.settings.indicators.forEach((i) => this.removeIndicatorCSS(i))
-    }
-
-    addIndicatorCSS(indicator: Indicator) {
-        const item = document.querySelector(".workspace-leaf-content[data-type='file-explorer'] .tree-item-self[data-path='" + indicator.dataPath + "']>.tree-item-inner");
-        item?.addClass("indicator");
-        if(indicator.shape == "SQUARE") item?.addClass("indicator-square");
-        if(indicator.shape == "SQUIRCLE") item?.addClass("indicator-squircle");
-        item?.setAttribute("style", "--indicator-color: " + indicator.color);
-    }
-
-    removeIndicatorCSS(indicator: Indicator) {
-        const item = document.querySelector(".workspace-leaf-content[data-type='file-explorer'] .tree-item-self[data-path='" + indicator.dataPath + "']>.tree-item-inner");
-        item?.removeClass("indicator", "indicator-square", "indicator-quircle");
-        item?.removeAttribute("style");
-    }
-}
-
-class FileIndicatorsSettingTab extends PluginSettingTab {
+export default class FileIndicatorsSettingTab extends PluginSettingTab {
 	plugin: FileIndicatorsPlugin;
 
 	constructor(app: App, plugin: FileIndicatorsPlugin) {
@@ -214,9 +116,7 @@ class FileIndicatorsSettingTab extends PluginSettingTab {
             button.onClick(async () => {
                 setting.clear();
                 setting.settingEl.remove();
-                this.plugin.removeIndicatorCSS(indicator);
-                this.plugin.settings.indicators.remove(indicator);
-                await this.plugin.saveSettings();
+                this.plugin.removeIndicator(indicator);
             })
             .setTooltip('Delete indicator')
             .setIcon('trash-2');
---
+++ src/main.ts
@@ -0,0 +1,108 @@
+import { App, Plugin, PluginSettingTab, Setting, } from 'obsidian';
+
+import Indicator from './indicator';
+import FileIndicatorsSettingTab from './SettingTab';
+
+interface FileIndicatorsSettings {
+	defaultColor: string;
+    defaultShape: string;
+    indicators: (Indicator)[]
+}
+
+const DEFAULT_SETTINGS: FileIndicatorsSettings = {
+	defaultColor: '#8A5CF5',
+    defaultShape: 'CIRCLE',
+    indicators: [],
+}
+
+export default class FileIndicatorsPlugin extends Plugin {
+	settings: FileIndicatorsSettings;
+
+	async onload() {
+		await this.loadSettings();
+        
+        this.loadIndicators();
+
+        this.registerEvent(this.app.workspace.on("file-menu", (menu, file) => {
+            const indicator = this.settings.indicators.find((indicator) => indicator.dataPath == file.path);
+
+            if(!indicator) {
+                menu.addItem(item => item
+                    .setTitle("Add indicator")
+                    .setIcon("plus")
+                    .onClick(async () => {
+                        const indicator = {
+                            dataPath: file.path,
+                            color: this.settings.defaultColor,
+                            shape: this.settings.defaultShape,
+                        };
+        
+                        this.addIndicator(indicator);
+                    }));
+            } else {
+                menu.addItem(item => item
+                    .setTitle("Remove indicator")
+                    .setIcon("minus")
+                    .onClick(async () => {
+                        const indicator = {
+                            dataPath: file.path,
+                            color: this.settings.defaultColor,
+                            shape: this.settings.defaultShape,
+                        };
+        
+                        this.removeIndicator(indicator);
+                    }));
+            }
+        }));
+
+		this.addSettingTab(new FileIndicatorsSettingTab(this.app, this));
+	}
+
+	onunload() {
+        this.unloadIndicators();
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
+    loadIndicators() {
+        this.settings.indicators.forEach((i) => this.addIndicatorCSS(i))
+    }
+    
+    unloadIndicators() {
+        this.settings.indicators.forEach((i) => this.removeIndicatorCSS(i))
+    }
+
+    async addIndicator(indicator: Indicator) {
+        this.addIndicatorCSS(indicator);
+        this.settings.indicators.push(indicator);
+        await this.saveSettings();
+    }
+
+    async removeIndicator(indicator: Indicator) {
+        this.removeIndicatorCSS(indicator);
+        const index = this.settings.indicators.findIndex((i) => i.dataPath == indicator.dataPath);
+        this.settings.indicators.remove(this.settings.indicators[index])
+        await this.saveSettings();
+    }
+
+    addIndicatorCSS(indicator: Indicator) {
+        const item = document.querySelector(".workspace-leaf-content[data-type='file-explorer'] .tree-item-self[data-path='" + indicator.dataPath + "']>.tree-item-inner");
+        item?.addClass("indicator");
+        if(indicator.shape == "SQUARE") item?.addClass("indicator-square");
+        if(indicator.shape == "SQUIRCLE") item?.addClass("indicator-squircle");
+        item?.setAttribute("style", "--indicator-color: " + indicator.color);
+    }
+
+    removeIndicatorCSS(indicator: Indicator) {
+        const item = document.querySelector(".workspace-leaf-content[data-type='file-explorer'] .tree-item-self[data-path='" + indicator.dataPath + "']>.tree-item-inner");
+        item?.removeClass("indicator", "indicator-square", "indicator-quircle");
+        item?.removeAttribute("style");
+    }
+}
+
